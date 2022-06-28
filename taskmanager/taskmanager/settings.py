19c243279edf5ee0d6e595d6a86e79c3353c4d2f
White-prince <tiltedfear@gmail.com>
2022-06-28 21:58:51 +0300

    edit page

--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5($k0ef$kky73q052!9_3vr=b##e&l(&&ky8m3sj&kdick#tdj'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
+   'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
@@ -116,7 +116,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = '/home/c/cf77097/vp/public_html/static/'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
@@ -128,14 +127,12 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-RECIPIENTS_EMAIL = ['alekceev101@gmail.com']
-DEFAULT_FROM_EMAIL = 'tiltedfear@gmail.com'
+RECIPIENTS_EMAIL = ['']
+DEFAULT_FROM_EMAIL = ''
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.mailgun.org'
-
-EMAIL_HOST_USER = 'postmaster@sandboxff81d06a1e11486bb7fe5cde20c889bb.mailgun.org'
-EMAIL_HOST_PASSWORD = 'ec0d8e5913938bdb544b18b8bd8683cf-4f207195-40debe5d'
-
-EMAIL_PORT = 587
-EMAIL_USE_TLS = False
+EMAIL_BACKEND = ''
+EMAIL_HOST = ''
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 
+EMAIL_USE_TLS = True
